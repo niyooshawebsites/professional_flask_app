645357796973e4540635df43b1c3b25d1a9166f1
niyooshawebsites <niyooshawebsites@gmail.com>
2026-08-17 00:45:19 +0530

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from .config import Config
+from .extensions import db, csrf
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    db.init_app(app)
+    csrf.init_app(app)
+    
+    from .main.routes import main_bp
+    from .auth.routes import auth_bp
+    
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
+    
+    return app
